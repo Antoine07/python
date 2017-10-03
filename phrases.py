b77633becf289f39b22e4771446b2eb4099b911d
Antoine07 <antoine.lucsko@wanadoo.fr>
2017-10-03 21:06:07 +0200

    first commit

---
+++ phrases.py
@@ -0,0 +1,1 @@
+str = "En Python une chaine de caractere est un objet, bien que l'on ne puisse pas encore definir exactement cette notion, retenez qu'un objet possede des methodes. Ils sont en Python egalement iterable. Soit donc la chaine de caracteres suivante, compter le nombre de fois qu'apparaît la lettre e  dans l'enomcer de notre exercice."
