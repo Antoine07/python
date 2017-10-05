d86fa586a374257b218da8fb9aaf82289125d0c1
yukii <32541696+yukii@users.noreply.github.com>
2017-10-05 13:40:53 +0200

    Update exercice16_bis.py

--- Part4/exercice16_bis.py
+++ Part4/exercice16_bis.py
@@ -10,15 +10,10 @@ for l in range(N):
     for c in range(N):
         if l == 0 or l == (N-1) or (c == 0 or c == (N-1)):
             sym = "X"
-        else:
-            if l % 2 == 0:
+        elif (l + c) % 2 == 0:
                 sym = "#"
-                if c % 2 == 0:
-                    sym = "0"
             else:
                 sym = "0"
-                if c % 2 == 0:
-                    sym  = "#"
         print(sym, end="")
     print()
 
