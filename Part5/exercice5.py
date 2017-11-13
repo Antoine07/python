edee1c72c658e565fbd87fa83bbb98890615b31b
Antoine07 <antoine.lucsko@wanadoo.fr>
2017-11-13 11:20:30 +0100

    décrypatage et cryptage avec sens

--- Part5/exercice5.py
+++ Part5/exercice5.py
@@ -18,7 +18,9 @@ def gen_key(u0,a,b):
     return vectKey
 
 # crypto
-def cryp_message(message, key):
+# le paramètre sens est facultatif car on lui assigne une valeur par défaut
+# on peut écraser cette valeur en appelant la fonction
+def cryp_message(message, key, sens = 1):
     message_crypt = ''
     length_key = len(key)
     i = 0
@@ -27,7 +29,7 @@ def cryp_message(message, key):
             message_crypt += ' '
             continue # permet d'aller à l'itération suivante
 
-        pos = ( LETTERS.index(ch) + key[i % length_key] ) % 26
+        pos = ( LETTERS.index(ch) + sens*key[i % length_key] ) % 26
         message_crypt +=  LETTERS[pos] 
 
         i += 1
@@ -40,5 +42,7 @@ key = gen_key(U0,A,B) # clé de codage
 print(key)
 
 message = "HELLO WORLD"
-message_cryp = cryp_message(message, key) # 
+message_cryp = cryp_message(message, key) 
 print(message_cryp)
+
+print(cryp_message(message_cryp, key, -1))
